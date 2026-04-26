2a3503155816ce6934f0b0e04f6e2e3e9e1dbf59
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-26 22:21:00 +0300

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    //Comments in C are very simple.
+    //There are two types of comments in total.
+    //Single-line and multi-line.
+    //This is single-line comment
+    /*
+    This is
+    multi-line
+    comment
+    To start a one-sided comment, just write // 
+    anywhere in the line, you can even do it like this
+    */
+    return 0;
+}
+    //To start a multi-line, you need to designate
+    // /* as the beginning and */ as the end, and between
+    //them you can write at least 1000 lines
+    //there is no difference
+    
