23270d2eded971101ee1d999b99681fb3f23a902
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-26 18:43:10 +0300

    Create basics.c

---
+++ printf/basics.c
@@ -0,0 +1,21 @@
+#include <stdio.h> //important for printf
+
+int main() {
+  printf("Hello World!"); //printf
+  //Int main is the entry point to a C
+  //program, it's required unless you're 
+  //writing a kernel.
+  //; required
+  //int is the type of data being returned
+  //it must be specified in functions if the 
+  //function will return something, for 
+  //example a + b. 
+  return 0;
+  //It's like 0 errors and that's cool.
+} 
+//This marks the end of the main function,
+//i.e. the entry into the program, as you can
+//see { at the beginning } at the end
+//this must be indicated
+//otherwise there will be an error
+
