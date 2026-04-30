4fccd84ff2557bdb97fadd59d49606102f45af1a
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-04-30 17:30:22 +0300

    Add scope.c to illustrate variable scope in C
    This file demonstrates the use of global and local variables in C, including their declaration and scope.

---
+++ variables/scope.c
@@ -0,0 +1,57 @@
+//scope.c
+/*
+Global variables are variables
+that are not in a function and
+are visible to everyone
+*/
+/*
+and the local one is the
+one that is located, for
+example, in the main function
+and any other, that is, the one
+that is located inside {}
+*/
+/*
+both global and local variables
+are declared like this: datatype name = value;
+*/
+#include <studio.h>
+
+int global = 25; //global variable
+
+int main() {
+    int local = 50; //local
+    return 0;
+}
+/*
+The advantage of global
+variables is that they
+can be used in any function.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
